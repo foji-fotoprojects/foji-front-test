c47020ff415de667641561d0b36c59aaf6ed861d
Dafnochka <dlushpina@gmail.com>
2019-03-25 00:25:34 +0300

    test page

---
+++ precache-manifest.7fdec18c5afeb98ad7ce7264f1ac6c3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5a6b71255dc832da585d5c94fa1a115",
+    "url": "/static/media/RobotoCondensed-Regular.a5a6b712.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a380d7184dfa50c8ac2d",
+    "url": "/static/js/main.a380d718.chunk.js"
+  },
+  {
+    "revision": "a2f1744582788f2fc630",
+    "url": "/static/js/2.a2f17445.chunk.js"
+  },
+  {
+    "revision": "a380d7184dfa50c8ac2d",
+    "url": "/static/css/main.2b9d16a2.chunk.css"
+  },
+  {
+    "revision": "e5aefb263c4a561203e4ce5d5eaf2409",
+    "url": "/index.html"
+  }
+];
